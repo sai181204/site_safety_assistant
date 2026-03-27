6b9dc8bf0ec1bb9ed068c21644c10fe4a11443c5
sai181204 <187545837+sai181204@users.noreply.github.com>
2026-03-28 00:55:14 +0530

    Update home route to render index.html

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚧 Smart Construction Safety System is Running!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
